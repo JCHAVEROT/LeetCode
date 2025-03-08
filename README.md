b95c88ddb8c6549d9dbce50692bda003e016bcd0
Jeremy Chaverot <100281310+jchaverot@users.noreply.github.com>
2025-03-08 13:36:34 +0100

    Add solution #27 with `RemoveElement.cpp`

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # My own solutions to Leetcode problems
-=======================================
+
 
 ## Algorithms
 
 | # | Title | Solution | Difficulty |
-| 88 | [Merge Sort Arrays](https://leetcode.com/problems/merge-sorted-array) | [C++](tbd) | Easy |
+| - | ----- | -------- | ---------- |
+| 88 | [Merge Sort Arrays](https://leetcode.com/problems/merge-sorted-array) | [C++](https://github.com/JCHAVEROT/leetcode/blob/main/algorithms/MergeSortArrays.cpp) | Easy |
+| 27 | [Remove Element](https://leetcode.com/problems/remove-element) | [C++](https://github.com/JCHAVEROT/leetcode/blob/main/algorithms/RemoveElement.cpp) | Easy |
---
+++ algorithms/RemoveElement.cpp
@@ -0,0 +1,53 @@
+// File   : RemoveElement.cpp
+// Source : https://leetcode.com/problems/remove-element
+// Author : Jérémy Chaverot
+// Date   : 01-03-2025
+
+/********************************************************************************
+ * 
+ * Given an integer array nums and an integer val, remove all occurrences of val 
+ * in nums in-place. The order of the elements may be changed. Then return the 
+ * number of elements in nums which are not equal to val.
+ * 
+ * Consider the number of elements in nums which are not equal to val be k, to get 
+ * accepted, you need to do the following things:
+ * 
+ * Change the array nums such that the first k elements of nums contain the 
+ * elements which are not equal to val. The remaining elements of nums are not 
+ * important as well as the size of nums. Return k.
+ * 
+ ********************************************************************************/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    // Complexity: O(n²) time, O(1) space
+    int removeElement(vector<int>& nums, int val) {
+        // Empty check
+        if (nums.empty()) { return 0; }
+
+        int size = nums.size();
+        int k = 0;
+
+        // Run trough the array backwards
+        for (int i = size-1; i >= 0; i--) {
+
+            // Check the remove condition
+            if (nums[i] == val) {
+
+                // Shift values in the array
+                for (int j = i; j < size-1; j++) {
+                    nums[j] = nums[j+1];
+                }
+
+            // Increment the counter k otherwise 
+            } else {
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
